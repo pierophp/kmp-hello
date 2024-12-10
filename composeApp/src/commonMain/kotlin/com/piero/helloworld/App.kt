51bc6978c1b1fcdb030b1ac8758a7863b7dc805b
Piero Giusti <pigiusti@bethel.jw.org>
2024-12-10 15:04:17 -0300

    Forst component

--- composeApp/src/commonMain/kotlin/com/piero/helloworld/App.kt
+++ composeApp/src/commonMain/kotlin/com/piero/helloworld/App.kt
@@ -4,12 +4,18 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Badge
+import androidx.compose.material.BadgedBox
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -23,7 +29,22 @@ fun App() {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+                Text("Click me2!")
+            }
+            BadgedBox(
+                badge = {
+                    Badge(
+                        backgroundColor = Color.Red,
+                        contentColor = Color.White
+                    ) {
+                         Text("5") // Example badge content
+                    }
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Email,
+                    contentDescription = "Email"
+                )
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
